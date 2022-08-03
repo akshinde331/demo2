c2afc0781b005b74670e658ecdf7654a53bdc1d9
akshinde331 <akshinde331@gmail.com>
2022-08-03 13:13:28 +0530

    new nishith

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,32 @@
+<<<<<<< HEAD
 
 hii 
+=======
+
+
+import java.util.Scanner;
+
+public class Calculator1 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter Number=");
+		
+		//Scanner s = new Scanner(System.in);
+		int a, b, sum=0, div=0;
+		
+		a = 40;
+		b = 20;
+		
+		
+		sum = a+b;
+		System.out.println("sum = "+sum);
+		
+		div = a-b;
+		System.out.println("div = "+div);
+	}
+
+}
+>>>>>>> d3ff1c3828c5a0a81f95fd0438b356f870e1a42e
