1404d3d22eeddadcfbd06892fa1cccd3330655db
akshinde331 <107904979+akshinde331@users.noreply.github.com>
2022-08-18 23:32:58 +0530

    Update Calculator - Copy.java

--- Calculator - Copy.java
+++ Calculator - Copy.java
@@ -1,27 +1,14 @@
 
 
-import java.util.Scanner;
-
 public class Calculator {
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Enter Number=");
-		
-		//Scanner s = new Scanner(System.in);
-		int a, b, sum=0, div=0;
-		
-		a = 40;
-		b = 20;
+		System.out.println("Hello World");
 		
 		
-		sum = a+b;
-		System.out.println("sum = "+sum);
-		
-		div = a-b;
-		System.out.println("div = "+div);
 	}
 
 }
