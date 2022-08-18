419133a412305d8c2fab5796de1d6b504d5e6c7a
akshinde331 <akshinde331@gmail.com>
2022-08-18 23:15:23 +0530

    Akshay Shinde

---
+++ Calculator - Copy.java
@@ -0,0 +1,29 @@
+
+
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter Number=");
+		
+		//Scanner s = new Scanner(System.in);
+		int a, b, sum=0, div=0;
+		
+		a = 40;
+		b = 20;
+		
+		
+		sum = a+b;
+		System.out.println("sum = "+sum);
+		
+		div = a-b;
+		System.out.println("div = "+div);
+	}
+
+}
+
